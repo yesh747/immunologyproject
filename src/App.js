5cdc73118422bfae555c1d66c7531c0eeec3f274
Yeshwant Chillakuru <Yesh@Yeshwants-MacBook-Pro.local>
2017-04-11 21:40:43 -0400

    reddit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 import Scroll from 'react-scroll'
+import {Reddit} from './redditComponent'
+
 
 class Header extends Component {
   render() {
@@ -12,9 +14,9 @@ class Header extends Component {
 
     return (
         <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2><span style={style_header}>Cancer Immunotherapy</span><br />for Struggling Immunology Students</h2>
-    </div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2><span style={style_header}>Cancer Immunotherapy</span><br />for Struggling Immunology Students</h2>
+        </div>
 
   );
   }
@@ -25,13 +27,14 @@ class Nav extends Component {
 
     return (
         <div className="App-nav">
-        <Scroll.Link activeClass="active" to="#buckleybonus" spy={true} smooth={true} duration={500} offset={-70}>Buckley Bonus!</Scroll.Link>
-    <Scroll.Link activeClass="active" to="#twitter" spy={true} smooth={true} duration={500} offset={-70}>Twitter for Immuno?</Scroll.Link>
-    <Scroll.Link activeClass="active" to="#topic4" spy={true} smooth={true} duration={500} offset={-70}>Topic 4</Scroll.Link>
-    <Scroll.Link activeClass="active" to="#topic3" spy={true} smooth={true} duration={500} offset={-70}>Topic 3</Scroll.Link>
-    <Scroll.Link activeClass="active" to="#topic2" spy={true} smooth={true} duration={500} offset={-70}>Topic 2</Scroll.Link>
-    <Scroll.Link activeClass="active" to="#topic1" spy={true} smooth={true} duration={500} offset={-70}>Topic 1</Scroll.Link>
-    </div>
+            <Scroll.Link activeClass="active" to="#buckleybonus" spy={true} smooth={true} duration={500} offset={-70}>Buckley Bonus!</Scroll.Link>
+            <Scroll.Link activeClass="active" to="#tweets" spy={true} smooth={true} duration={500} offset={-70}>Trending Twitter</Scroll.Link>
+            <Scroll.Link activeClass="active" to="#reddit" spy={true} smooth={true} duration={500} offset={-70}>Reddit Reads</Scroll.Link>
+            <Scroll.Link activeClass="active" to="#topic4" spy={true} smooth={true} duration={500} offset={-70}>Topic 4</Scroll.Link>
+            <Scroll.Link activeClass="active" to="#topic3" spy={true} smooth={true} duration={500} offset={-70}>Topic 3</Scroll.Link>
+            <Scroll.Link activeClass="active" to="#topic2" spy={true} smooth={true} duration={500} offset={-70}>Topic 2</Scroll.Link>
+            <Scroll.Link activeClass="active" to="#topic1" spy={true} smooth={true} duration={500} offset={-70}>Topic 1</Scroll.Link>
+        </div>
   );
   }
 }
@@ -73,7 +76,10 @@ class Content extends Component {
             <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</p>
             <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
             <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.</p>
-            </div>
+        </div>
+        <div className="reddit" id="#reddit">
+            <Reddit subreddit="Immunology"/>
+        </div>
     </div>
     )
   }
---
+++ src/redditComponent.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Yesh on 4/11/17.
+ */
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class Reddit extends Component {
+		constructor(props) {
+				super(props);
+
+				this.state = {
+						posts: []
+				};
+		}
+
+		componentDidMount() {
+				axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
+						.then(res => {
+
+								const posts = res.data.data.children.map(obj => obj.data);
+								this.setState({ posts });
+						});
+		}
+
+		render() {
+				return (
+						<div>
+								<h1>{`/r/${this.props.subreddit}`}</h1>
+								<ul>
+										{this.state.posts.map(post =>
+												<li key={post.id}><a href={post.url}>{post.title}</a></li>
+										)}
+								</ul>
+						</div>
+				);
+		}
+}
+
+export {Reddit}
